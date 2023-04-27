c287b3d802a0393cc4006e9e4b5e1f9140d0b538
AnastasiaLaryushkina <alaryushkina@gmail.com>
2023-04-27 18:12:28 +0300

    update

---
+++ src/main/java/ru/itgirl/libraryproject/dto/AuthorsAndBooksResponseDto.java
@@ -0,0 +1,20 @@
+package ru.itgirl.libraryproject.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthorsAndBooksResponseDto {
+    String genre;
+    List <AuthorDto> authors;
+
+}
